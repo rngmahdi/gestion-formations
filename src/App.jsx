2d9cd5e56ca5ef2bd1b8dadac082aee6ff6d25b7
RNGMAHDI <rngmahdi123@gmail.com>
2024-02-20 10:39:46 +0100

    added json databas and some dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Example from './components/Example'
+import { useState } from "react";
+import Stagiaire from "./components/Stagiaire";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
-     <Example />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
