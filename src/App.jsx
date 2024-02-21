217626f8fbd3bbb19bda0ba285881ce7b6ac0adc
RNGMAHDI <rngmahdi123@gmail.com>
2024-02-21 15:19:54 +0100

    added content to employe page from data

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import { useState } from "react";
-import Stagiaire from "./pages/Stagiaire";
+import Stagiaire from "./pages/Employee.jsx";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./layouts/Layout.jsx";
 import Formation from "./pages/Formation.jsx";
 import Participation from "./pages/Participation.jsx";
+import Employee from "./pages/Employee.jsx";
+import EditEmployee from "./pages/employee/EditEmployee.jsx";
 function App() {
   const [count, setCount] = useState(0);
 
@@ -12,7 +14,8 @@ function App() {
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<Layout />}>
-            <Route index element={<Stagiaire />} />
+            <Route index element={<Employee />} />
+            <Route path="/employee/:id" element={<EditEmployee />} />
             <Route path="formations" element={<Formation />} /> 
             <Route path="participations" element={<Participation />} /> 
           </Route>
--- src/pages/Employee.jsx
+++ src/pages/Employee.jsx
@@ -1,10 +1,17 @@
-import React, { useEffect } from "react";
-
-export default function Stagiaire() {
-  useEffect(() => {}, []);
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+export default function Employee() {
+  let [employees, setEmployees] = useState([]);
+  useEffect(() => {
+    axios.get("http://localhost:8000/stagiaires").then((res) => {
+      // const employees = res.data
+      setEmployees(res.data);
+      console.log(employees);
+    });
+  }, []);
   return (
     <div>
-      <section className="items-center lg:flex bg-gray-50 lg:h-screen font-poppins dark:bg-gray-800 ">
+      <section className="items-center lg:flex bg-gray-50  font-poppins dark:bg-gray-800 ">
         <div className="justify-center flex-1 max-w-6xl px-4 py-4 mx-auto lg:py-8 md:px-6">
           <div className="pt-4 bg-white rounded shadow dark:bg-gray-900">
             <div className="flex px-6 pb-4 border-b dark:border-gray-700">
@@ -15,32 +22,51 @@ export default function Stagiaire() {
             <div className="p-4 overflow-x-auto">
               <table className="w-full table-auto">
                 <thead>
-                  <tr className="text-xs text-left text-gray-500 dark:text-gray-400">
+                  <tr className="text-xs text-center text-gray-500 dark:text-gray-400">
                     <th className="px-6 pb-3 font-medium">ID</th>
                     <th className="px-6 pb-3 font-medium">Nom </th>
-                    <th className="px-6 pb-3 font-medium">Prenom </th>
                     <th className="px-6 pb-3 font-medium">Age</th>
                     <th className="px-6 pb-3 font-medium">Telephone</th>
                     <th className="px-6 pb-3 font-medium">Formations</th>
+                    <th className="px-6 pb-3 font-medium">Actions</th>
                   </tr>
                 </thead>
                 <tbody>
+                  {employees.map((employee) => {
+                    return (
+                      <tr className="text-xs text-center bg-gray-100 dark:text-gray-400 dark:bg-gray-800">
+                        <td className="px-6 py-5 font-medium">{employee.id}</td>
+                        <td className="px-6 py-5 font-medium">
+                          {employee.prenom} {employee.nom}
+                        </td>
+                        <td className="px-6 py-5 font-medium">
+                          {employee.age}
+                        </td>
+                        <td className="px-6 py-5 font-medium">
+                          {employee.telephone}
+                        </td>
+                        <td className="px-6 py-5 font-medium">
+                          <ul className="list-disc text-left">
+                            {employee.formations.map((formation) => {
+                              return (
+                              <li className="py-1">
+                                {formation.title}
+                              </li>
+                              );
+                            })}
+                          </ul>
+                        </td>
+                      </tr>
+                    );
+                  })}
                   <tr className="text-xs bg-gray-100 dark:text-gray-400 dark:bg-gray-800">
                     <td className="px-6 py-5 font-medium">018276td45</td>
                     <td className="px-6 py-5 font-medium ">08.4.2021</td>
                     <td className="px-6 py-5 font-medium ">abc@gmail.com</td>
                     <td className="px-6 py-5 font-medium ">abc@gmail.com</td>
                     <td className="px-6 py-5 font-medium ">abc@gmail.com</td>
-                    <td>
-                      <span className="inline-block px-2 py-1 text-center text-green-600 bg-green-100 rounded-full dark:text-green-700 dark:bg-green-200">
-                        Completed
-                      </span>
-                    </td>
                     <td className="px-6 py-5 flex gap-4">
-                      <a
-                        href="#"
-                        className=" font-medium text-blue-500  group"
-                      >
+                      <a href="#" className=" font-medium text-blue-500  group">
                         <svg
                           xmlns="http://www.w3.org/2000/svg"
                           viewBox="0 0 24 24"
@@ -80,60 +106,6 @@ export default function Stagiaire() {
                             d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
                           />
                         </svg>
-                      </a>
-                    </td>
-                  </tr>
-                  <tr className="text-xs dark:text-gray-400">
-                    <td className="px-6 py-5 font-medium">018276td45</td>
-                    <td className="px-6 py-5 font-medium ">08.4.2021</td>
-                    <td className="px-6 py-5 font-medium ">abc@gmail.com</td>
-                    <td>
-                      <span className="inline-block px-2 py-1 text-center text-yellow-600 bg-yellow-100 rounded-full dark:text-yellow-700 dark:bg-yellow-200">
-                        Pending
-                      </span>
-                    </td>
-                    <td className="px-6 py-5 ">
-                      <a
-                        href="#"
-                        className="px-4 py-2 font-medium text-blue-500 border border-blue-500 rounded-md dark:text-blue-300 dark:border-blue-300 dark:hover:bg-blue-300 dark:hover:text-gray-700 hover:text-gray-100 hover:bg-blue-500"
-                      >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                  <tr className="text-xs bg-gray-100 dark:bg-gray-800 dark:text-gray-400">
-                    <td className="px-6 py-5 font-medium">018276td45</td>
-                    <td className="px-6 py-5 font-medium ">08.4.2021</td>
-                    <td className="px-6 py-5 font-medium ">abc@gmail.com</td>
-                    <td>
-                      <span className="inline-block px-2 py-1 text-center text-green-600 bg-green-100 rounded-full dark:text-green-700 dark:bg-green-200">
-                        Completed
-                      </span>
-                    </td>
-                    <td className="px-6 py-5 ">
-                      <a
-                        href="#"
-                        className="px-4 py-2 font-medium text-blue-500 border border-blue-500 rounded-md dark:text-blue-300 dark:border-blue-300 dark:hover:bg-blue-300 dark:hover:text-gray-700 hover:text-gray-100 hover:bg-blue-500"
-                      >
-                        Edit
-                      </a>
-                    </td>
-                  </tr>
-                  <tr className="text-xs dark:text-gray-400">
-                    <td className="px-6 py-5 font-medium">018276td45</td>
-                    <td className="px-6 py-5 font-medium ">08.4.2021</td>
-                    <td className="px-6 py-5 font-medium ">abc@gmail.com</td>
-                    <td>
-                      <span className="inline-block px-2 py-1 text-center text-red-600 bg-red-100 rounded-full dark:text-red-700 dark:bg-red-200">
-                        Cancelled
-                      </span>
-                    </td>
-                    <td className="px-6 py-5 ">
-                      <a
-                        href="#"
-                        className="px-4 py-2 font-medium text-blue-500 border border-blue-500 rounded-md dark:text-blue-300 dark:border-blue-300 dark:hover:bg-blue-300 dark:hover:text-gray-700 hover:text-gray-100 hover:bg-blue-500"
-                      >
-                        Edit
                       </a>
                     </td>
                   </tr>
