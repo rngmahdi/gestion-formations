c3cc1925ccb38d468d357563b52ea2f5b95aa7e0
RNGMAHDI <rngmahdi123@gmail.com>
2024-03-25 21:56:56 +0000

    removed uneccesairy reducers

--- src/redux/reducers.jsx
+++ src/redux/reducers.jsx
@@ -21,48 +21,39 @@ const initialState = {
   assignFormVisibility: false,
 };
 export default function reducer(state = initialState, action) {
-   switch (action.type) {
-     //* Employee
- 
-     case "setEmployees":
-       return { ...state, employees: action.payload };
- 
-     case "setEmployeeFormType":
-       return { ...state, employeeFormType: action.payload };
- 
-     case "setSelectedEmployee":
-       return { ...state, selectedEmployee: action.payload };
- 
-     case "setEmployeeFormVisibility":
-       return { ...state, employeeFormVisibility: action.payload };
- 
-     //* Formation
- 
-     case "setFormations":
-       return { ...state, formations: action.payload };
- 
-     case "setFormationFormType":
-       return { ...state, formationFormType: action.payload };
- 
-     case "setSelectedFormation":
-       return { ...state, selectedFormation: action.payload };
- 
-     case "setFormationFormVisibility":
-       return { ...state, formationFormVisibility: action.payload };
- 
-     //* Assign
-     case "setSelectedFormationAssign":
-       return { ...state, selectedFormationAssign: action.payload };
- 
-     case "setAssignFormType":
-       return { ...state, assignFormType: action.payload };
- 
-     case "setAssignFormVisibility":
-       return { ...state, assignFormVisibility: action.payload };
- 
-     default:
-       return state;
-   }
- }
+  switch (action.type) {
+    //* Employee
+
+    case "setEmployees":
+      return { ...state, employees: action.payload };
+
+    case "setEmployeeFormType":
+      return { ...state, employeeFormType: action.payload };
+
+    case "setSelectedEmployee":
+      return { ...state, selectedEmployee: action.payload };
+
+    //* Formation
+
+    case "setFormations":
+      return { ...state, formations: action.payload };
+
+    case "setFormationFormType":
+      return { ...state, formationFormType: action.payload };
+
+    case "setSelectedFormation":
+      return { ...state, selectedFormation: action.payload };
+
+    //* Assign
+    case "setSelectedFormationAssign":
+      return { ...state, selectedFormationAssign: action.payload };
+
+    case "setAssignFormType":
+      return { ...state, assignFormType: action.payload };
+
+    default:
+      return state;
+  }
+}
 
 // export default reducer;
